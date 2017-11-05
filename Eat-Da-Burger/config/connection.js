79f8adc70913442f7d6684a5e57b7fb4dfbc64b4
bianca-montoya <bmontoya@toastmasters.org>
2017-11-04 22:41:20 -0700

    adding NYT React app

--- Eat-Da-Burger/config/connection.js
+++ Eat-Da-Burger/config/connection.js
@@ -8,10 +8,10 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: "d6ybckq58s9ru745.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "uvug6bh5kd59ok3o",
+  password: "q15ukl734iqrbus4",
+  database: "usr6eb072pxjtozk"
 });
 };
 
